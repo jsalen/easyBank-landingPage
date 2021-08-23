576829d42ea77026f51025428be515fecb23f8ea
Joseph Salen <jsalen.91@gmail.com>
2021-08-23 19:48:13 -0400

    fixed hero responsiveness

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import Button from "../Button/Button.jsx";
 
-import heroBackground from "../../statics/images/bg-intro-mobile.svg";
-import mockups from "../../statics/images/image-mockups.png";
 import "../../statics/styles/Hero.css";
 
 const Hero = () => {
   return (
     <section className="hero">
-      <div className="hero__image">
-        <img src={heroBackground} className="image__background" />
-        <img src={mockups} className="image__mockup" />
-      </div>
+      <div className="hero__image"></div>
       <div className="hero__cta">
         <h1>Next Generation digital banking</h1>
         <p>
